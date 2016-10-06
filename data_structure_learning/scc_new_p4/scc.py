6296ec233406ba4b29806b66be00c9bae6adabaa
zechen liu <zechen@zechen.local>
2016-10-05 20:48:44 -0700

    -a

--- data_structure_learning/scc_new_p4/scc.py
+++ data_structure_learning/scc_new_p4/scc.py
@@ -2,7 +2,12 @@ class SCC(object):
     def __init__(self):
         pass
 
-    def depth_first_search(self, data):
-        for node in data:
-            
-        
+    def depth_first_search(self, data, order_of_node=[]):
+        if order_of_node:
+            depth_first_search_rec        
+
+    def depth_first_search_rec(self, data, node, discovered_nodes, order_of_node=[]):
+        for connection in data:
+            if connection in discovered_nodes:
+                continue
+            s.add(connection)

128ae98fe6f20744a28bdb3975f023ba54908fff
zechen liu <zechen@zechen.local>
2016-10-16 15:38:44 -0700

    scc2

---
+++ data_structure_learning/scc_redos_p4/scc2.py
@@ -0,0 +1,49 @@
+class SCC(object):
+    def __init__(self):
+        self.finishing_time = 0
+
+    def scc(self, data):
+        data_r = self.reverse(data)
+        self.finishing_time = 0
+        finishing_time_dictionary = {}
+        leader_map = {}
+        visited_nodes = set()
+
+        for i in xrange(len(data)-1, -1, -1):
+            self.dfs_cf(data_r, i, visited_nodes, finishing_time_dictionary)
+
+        for i in xrange(len(data)-1, -1, -1):
+            self.dfs_cl(data, finishing_time_dictionary[i], visited_nodes, finishing_time_dictionary[i], leader_map)
+
+        return leader_map
+
+    def reverse(self, data):
+        data_r = []
+        for i in xrange(len(data)):
+            data_r.append([])
+
+        for node in data:
+            for connection in node:
+                data_r[connection].append(r)
+        return data_r
+
+    def dfs_cf(self, data, node, visited_nodes, finishing_time_dictionary):
+        if node not in visited_nodes:
+            visited_nodes.add(node)
+            for connection in data[node]:
+                self.dfs_cf(data, connection, visited_nodes, finishing_time_dictionary)
+            assert(self.finishing_time not in finishing_time_dictionary)
+            finishing_time_dictionary[self.finishing_time] = node
+            self.finishing_time += 1
+
+    def dfs_cl(self, data, node, visited_nodes, leader, leader_map):
+        if node not in visited_nodes:
+            visited_nodes.add(node)
+            if leader in leader_map:
+                leader_map[leader].append(node)
+            else:
+                assert(leader not in leader_map)
+                assert(node == leader)
+                leader_map[leader] = [node]
+            for connection in data[node]:
+                self.dfs_cl(data, connection, visited_nodes, leader, leader_map)
